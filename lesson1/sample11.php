821e600e4a71360c77a84e0b98abe550b1cb9096
naoya <naoya.take10.01@gmail.com>
2022-05-21 22:16:10 +0900

    連想配列

---
+++ lesson1/sample11.php
@@ -0,0 +1,17 @@
+<?php 
+  $fruits = [
+  'apple' =>'りんご',
+  'grape' => 'ぶどう',
+  'lemon' => 'レモン',
+  'tomato' => 'トマト',
+  'peach' => '桃'
+];
+  echo $fruits['lemon'];
+?>
+
+  <dl>
+    <?php foreach ($fruits as $english => $japanese): ?>
+    <dt><?php echo $english ?></dt>
+    <dd><?php echo $japanese ?></dd>
+    <?php endforeach; ?>
+  </dl>
