4e38f21116e4dbe5fdb2e60749cfe88e1223cae7
naoya <naoya.take10.01@gmail.com>
2022-06-07 12:36:49 +0900

    select

---
+++ lesson3/sample02.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>sample02</title>
+</head>
+<body>
+  <?php 
+    $db = new mysqli('localhost:8889','root', 'root','mydb');
+    /* new mysqli('host:ポート番号','username', 'password','データベース名') */
+    $records = $db->query('select * from my_items');
+    if ($records){
+     while ($record = $records->fetch_assoc()){
+       echo $record['item_name'] . '<br>';
+     }
+    } 
+  ?>
+</body>
+</html>
