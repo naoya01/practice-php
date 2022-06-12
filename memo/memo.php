ac8bf6812b5c2de6f14163b236db743e31dd7c1c
naoya <naoya.take10.01@gmail.com>
2022-06-12 13:30:18 +0900

    delete

--- memo/memo.php
+++ memo/memo.php
@@ -23,6 +23,7 @@
 
   <div><pre><?php echo htmlspecialchars($memo); ?></pre></div>
   <a href="update.php?id=<?php echo $id; ?>">編集する</a>|
+  <a href="delete.php?id=<?php echo $id; ?>">削除する</a>|
   <a href="/memo">一覧へ</a>
 </body>
 </html>
