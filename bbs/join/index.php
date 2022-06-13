59c9c70de29b2ea9d7ab7bf6247353337e6c95f0
naoya <naoya.take10.01@gmail.com>
2022-06-13 12:15:37 +0900

    board

--- bbs/join/index.php
+++ bbs/join/index.php
@@ -1,10 +1,45 @@
 <?php 
-    $form = [];
+    $form = [
+        'name' => '',
+        'email' => '',
+        'password' => ''
+
+    ];
     $error = [];
-    $form['name'] = filter_input(INPUT_POST,'name',FILTER_SANITIZE_STRING);
-    if ($form['name'] === ''){
-        $error['name'] = 'blank';
-    } 
+
+    /* htmlspecialcharsを短くする */
+    function h ($value){
+        return htmlspecialchars($value, ENT_QUOTES);
+    }
+
+    /* フォームの内容をチェック */
+    if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+        $form['name'] = filter_input(INPUT_POST,'name',FILTER_SANITIZE_STRING);
+        if ($form['name'] === ''){
+            $error['name'] = 'blank';
+        } 
+
+        $form['email'] = filter_input(INPUT_POST,'email',FILTER_SANITIZE_EMAIL);
+        if ($form['email'] === ''){
+            $error['email'] = 'blank';
+
+        } 
+        $form['password'] = filter_input(INPUT_POST,'password',FILTER_SANITIZE_STRING);
+        if ($form['password'] === ''){
+            $error['password'] = 'blank';
+        } elseif (strlen($form['password']) < 4 ) {
+            $error['password'] = 'length';
+        }
+
+        /* 画像のチェック */
+        $image = $_FILES['image'];
+        if ($image['name'] !== '' && $image['error'] === 0 ) {
+            $type = mime_content_type($image['tmp_name']);
+            if ($type !== 'image/png' && $type !== 'image/jpg'){
+                $error['image'] = 'type';
+            }
+        }
+     }
 ?>
 
 <!DOCTYPE html>
@@ -31,26 +66,34 @@
             <dl>
                 <dt>ニックネーム<span class="required">必須</span></dt>
                 <dd>
-                    <input type="text" name="name" size="35" maxlength="255" value="<?php echo htmlspecialchars($form['name'], ENT_QUOTES); ?>"/>
+                    <input type="text" name="name" size="35" maxlength="255" value="<?php echo h($form['name']); ?>"/>
                     <?php if (isset($error['name']) && $error['name'] === 'blank' ): ?>
                     <p class="error">* ニックネームを入力してください</p>
                     <?php endif; ?>
                 </dd>
                 <dt>メールアドレス<span class="required">必須</span></dt>
                 <dd>
-                    <input type="text" name="email" size="35" maxlength="255" value=""/>
+                    <input type="text" name="email" size="35" maxlength="255" value="<?php echo h($form['email']); ?>"/>
+                    <?php if (isset($error['email']) && $error['email'] === 'blank' ): ?>
                     <p class="error">* メールアドレスを入力してください</p>
+                    <?php endif; ?>
                     <p class="error">* 指定されたメールアドレスはすでに登録されています</p>
                 <dt>パスワード<span class="required">必須</span></dt>
                 <dd>
-                    <input type="password" name="password" size="10" maxlength="20" value=""/>
+                    <input type="password" name="password" size="10" maxlength="20" value="<?php echo h($form['password']); ?>"/>
+                    <?php if (isset($error['password']) && $error['password'] === 'blank' ): ?>
                     <p class="error">* パスワードを入力してください</p>
+                    <?php endif; ?>
+                    <?php if (isset($error['password']) && $error['password'] === 'length' ): ?>
                     <p class="error">* パスワードは4文字以上で入力してください</p>
+                    <?php endif; ?>
                 </dd>
                 <dt>写真など</dt>
                 <dd>
                     <input type="file" name="image" size="35" value=""/>
+                    <?php if (isset($error['image']) && $error['image'] ==='type'): ?>
                     <p class="error">* 写真などは「.png」または「.jpg」の画像を指定してください</p>
+                    <?php endif; ?>
                     <p class="error">* 恐れ入りますが、画像を改めて指定してください</p>
                 </dd>
             </dl>
